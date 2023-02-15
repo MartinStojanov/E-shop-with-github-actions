256fd863726501cc2a6dbc6f7c5d3f1e4103d5a3
MartinStojanov <martinstojanov6@gmail.com>
2023-02-15 23:45:10 +0100

    prometheus added

--- src/main/java/mk/ukim/finki/fashionstore/FashionStore.java
+++ src/main/java/mk/ukim/finki/fashionstore/FashionStore.java
@@ -6,11 +6,16 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
 
-@ServletComponentScan
+//@ServletComponentScan
 @SpringBootApplication
+@RestController
 public class FashionStore {
 
+    public String getMessage(){
+        return "Working ... ";
+    }
     public static void main(String[] args) {
         SpringApplication.run(FashionStore.class, args);
     }
